df8c35b80577e5b669f2724a7d585964479eae58
anaa125 <as20201042@student.fon.bg.ac.rs>
2022-09-14 21:10:46 +0200

    5 - resurs api rute i api kontroleri za kviz i takmicara

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KvizController;
+use App\Http\Controllers\TakmicarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('kviz', KvizController::class);
+Route::resource('takmicar', TakmicarController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
